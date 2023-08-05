614b4ebb2a6976c1bee64c3fea20920d9b13f7a0
= <=>
2023-08-06 00:11:25 +0530

    "Added tests for User API"

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+"""
+Views for the User API.
+"""
+from rest_framework import generics
+from user.serializers import UserSerializer
 
-# Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    """Create a New User in the System."""
+    serializer_class = UserSerializer
